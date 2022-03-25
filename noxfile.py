8cbbed6969c1da0214532411f7d51aa116769bc0
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2022-03-25 12:18:33 -0600

    chore: set up CI on v1 branch (#263)
    * chore: add basic CI to v1 branch
    
    * chore: try using nox -s cover
    
    * chore: go back to running cover directly
    
    * chore: fail under 45
    
    * chore: use sphinx 4
    
    * chore: use sphinx 2
    
    * chore: py 3.7 in GH workflows files
    
    * chore: add docfx
    
    * chore: format
    
    * chore: blacken
    
    Co-authored-by: Charles Engelke <github@engelke.com>

--- noxfile.py
+++ noxfile.py
@@ -45,7 +45,7 @@ def lint(session):
     session.run("flake8", "google", "tests")
 
 
-@nox.session(python="3.6")
+@nox.session(python=DEFAULT_PYTHON_VERSION)
 def blacken(session):
     """Run black.
 
@@ -149,7 +149,7 @@ def docs(session):
     """Build the docs for this library."""
 
     session.install("-e", ".")
-    session.install("sphinx<3.0.0", "alabaster", "recommonmark")
+    session.install("sphinx<3.0.0", "alabaster", "recommonmark", "Jinja2<3.1")
 
     shutil.rmtree(os.path.join("docs", "_build"), ignore_errors=True)
     session.run(
@@ -172,7 +172,11 @@ def docfx(session):
 
     session.install("-e", ".")
     session.install(
-        "sphinx<3.0.0", "alabaster", "recommonmark", "gcp-sphinx-docfx-yaml"
+        "sphinx<3.0.0",
+        "alabaster",
+        "recommonmark",
+        "gcp-sphinx-docfx-yaml",
+        "Jinja2<3.1",
     )
 
     shutil.rmtree(os.path.join("docs", "_build"), ignore_errors=True)
